48864ed5c2628152751b94b67e86630a24724964
Zuzanna Glowacka <zuzglowacka@gmail.com>
2018-05-06 00:11:49 +0100

    added Photo model

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180505193048) do
+ActiveRecord::Schema.define(version: 20180505230757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "photos", force: :cascade do |t|
     t.string "url"
-    t.string "string"
-    t.string "project_id"
-    t.string "bigint"
+    t.bigint "project_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
