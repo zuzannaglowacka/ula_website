38b23de86645cfa3bfab5a87c9168e225ecf64d4
Zuzanna Glowacka <zuzglowacka@gmail.com>
2018-05-10 22:51:04 +0100

    more cards added

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,17 @@ Project.destroy_all
 
 p "    destroying all projects..."
 
+
 p1 = Project.create(name: 'White', genre: 'Drama')
 Photo.create!(url: 'http://res.cloudinary.com/zuziac/image/upload/v1525451411/MV5BMTU2NjY3NDAwMF5BMl5BanBnXkFtZTcwOTc4OTcxMQ_._V1_.jpg', project: p1)
 
+p2 = Project.create(name: 'Blue', genre: 'Drama')
+Photo.create!(url: 'http://res.cloudinary.com/zuziac/image/upload/v1525788470/MV5BZGJhNDdmMWMtNzZiYy00MmFiLThjNmUtYTE1ZjRkNjdjYmQ5XkEyXkFqcGdeQXVyMTMxODk2OTU_._V1_.jpg', project: p2)
+
+p3 = Project.create(name: "Red", genre: 'Drama')
+Photo.create!(url: 'http://res.cloudinary.com/zuziac/image/upload/v1525987606/MV5BMTMzNjQ1Mjc0M15BMl5BanBnXkFtZTcwNDA0NjkxMQ_._V1_.jpg', project: p3)
+
+
 p "Coding Projects created !"
 
-# Photo.create!(url: 'http://res.cloudinary.com/zuziac/image/upload/v1525451411/MV5BMTU2NjY3NDAwMF5BMl5BanBnXkFtZTcwOTc4OTcxMQ_._V1_.jpg', project: p1)
+
